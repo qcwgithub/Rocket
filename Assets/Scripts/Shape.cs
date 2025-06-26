d3472f0f5c4b58545c0b8c3ef1601a83ddcb20a7
qcwgithub <doitmyway@qq.com>
2025-06-26 15:47:13 +0800

    Shape modify

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum Shape
@@ -23,9 +24,11 @@ public enum Shape
 
 public class ShapeSettings
 {
-    public Vector2Int[] linkedOffsets;
+    public List<Vector2Int> linkedOffsets;
     public bool linkedL;
     public bool linkedR;
+    public bool linkedT;
+    public bool linkedB;
     public Shape rotateCW;
     public Shape rotateCCW;
 }
@@ -48,207 +51,28 @@ public static class ShapeExt
 
     static ShapeSettings CreateSettings(this Shape e)
     {
-        switch (e)
+        ShapeSettings settings = new ShapeSettings();
+        settings.linkedOffsets = new List<Vector2Int>();
+
+        if (settings.linkedL = e.Linked_L())
         {
-            case Shape.L:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.R:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.right,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.T:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.up,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.B:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.LR:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                        Vector2Int.right,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.LT:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                        Vector2Int.up,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.LB:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.RT:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.right,
-                        Vector2Int.up,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.RB:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.right,
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.TB:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.up,
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.LRT:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                        Vector2Int.right,
-                        Vector2Int.up,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.LRB:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                        Vector2Int.right,
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.LTB:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                        Vector2Int.up,
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.RTB:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.right,
-                        Vector2Int.up,
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
-            case Shape.LRTB:
-            default:
-                return new ShapeSettings
-                {
-                    linkedOffsets = new Vector2Int[]
-                    {
-                        Vector2Int.left,
-                        Vector2Int.right,
-                        Vector2Int.up,
-                        Vector2Int.down,
-                    },
-                    linkedL = e.Linked_L(),
-                    linkedR = e.Linked_R(),
-                    rotateCW = e.RotateCW(),
-                    rotateCCW = e.RotateCCW(),
-                };
+            settings.linkedOffsets.Add(Vector2Int.left);
         }
+        if (settings.linkedR = e.Linked_R())
+        {
+            settings.linkedOffsets.Add(Vector2Int.right);
+        }
+        if (settings.linkedT = e.Linked_T())
+        {
+            settings.linkedOffsets.Add(Vector2Int.up);
+        }
+        if (settings.linkedB = e.Linked_B())
+        {
+            settings.linkedOffsets.Add(Vector2Int.down);
+        }
+        settings.rotateCW = e.RotateCW();
+        settings.rotateCCW = e.RotateCCW();
+        return settings;
     }
 
     static bool Linked_L(this Shape e)
@@ -319,6 +143,82 @@ public static class ShapeExt
                 return true;
             case Shape.LTB:
                 return false;
+            case Shape.RTB:
+                return true;
+            case Shape.LRTB:
+            default:
+                return true;
+        }
+    }
+
+    static bool Linked_T(this Shape e)
+    {
+        switch (e)
+        {
+            case Shape.L:
+                return false;
+            case Shape.R:
+                return false;
+            case Shape.T:
+                return true;
+            case Shape.B:
+                return false;
+            case Shape.LR:
+                return false;
+            case Shape.LT:
+                return true;
+            case Shape.LB:
+                return false;
+            case Shape.RT:
+                return true;
+            case Shape.RB:
+                return false;
+            case Shape.TB:
+                return true;
+            case Shape.LRT:
+                return true;
+            case Shape.LRB:
+                return false;
+            case Shape.LTB:
+                return true;
+            case Shape.RTB:
+                return true;
+            case Shape.LRTB:
+            default:
+                return true;
+        }
+    }
+
+    static bool Linked_B(this Shape e)
+    {
+        switch (e)
+        {
+            case Shape.L:
+                return false;
+            case Shape.R:
+                return false;
+            case Shape.T:
+                return false;
+            case Shape.B:
+                return true;
+            case Shape.LR:
+                return false;
+            case Shape.LT:
+                return false;
+            case Shape.LB:
+                return true;
+            case Shape.RT:
+                return false;
+            case Shape.RB:
+                return true;
+            case Shape.TB:
+                return true;
+            case Shape.LRT:
+                return false;
+            case Shape.LRB:
+                return true;
+            case Shape.LTB:
+                return true;
             case Shape.RTB:
                 return true;
             case Shape.LRTB:
